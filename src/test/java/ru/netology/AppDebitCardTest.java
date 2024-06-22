e1be8c9ecb0cc48b5ca6903127c211c9ced301ba
imshcheglov <Ivan_mix@mail.ru>
2024-06-22 23:49:41 +0500

    second try

--- src/test/java/ru/netology/AppDebitCardTest.java
+++ src/test/java/ru/netology/AppDebitCardTest.java
@@ -1,5 +1,6 @@
 package ru.netology;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,7 +19,8 @@ public class AppDebitCardTest {
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "driver/mac/chromedriver");
+        WebDriverManager.chromedriver().setup();
+//        System.setProperty("webdriver.chrome.driver", "driver/mac/chromedriver");
     }
 
     @BeforeEach
